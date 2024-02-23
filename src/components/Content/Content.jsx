df09091edc65ae0e53cc138a1221c386295622d6
GhanFa <ghani.fauzan22@gmail.com>
2024-02-23 20:23:14 +0700

    membuat sidebar-navbar dan layouting

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,11 @@
+import Breadcrumb from "../Breadcrumb/Breadcrumb";
+
+const Content = () => {
+  return (
+    <div className="content w-full bg-slate-200">
+      <Breadcrumb />
+    </div>
+  );
+};
+
+export default Content;
