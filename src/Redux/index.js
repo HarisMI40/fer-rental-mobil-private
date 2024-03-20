c97aaff99ebb96a6d4c91e449a51d39c15169e35
FaqihUlhaq <akmalfaqih770@gmail.com>
2024-03-21 00:31:16 +0700

    menambahkan authetication dan logout

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import carSlice from "./carSlice";
 import orderSlice from "./orderSlice";
+import authAdminSlice from "./admin-slice";
 
 const reducer = combineReducers({
   car: carSlice,
-  orders:orderSlice
+  orders: orderSlice,
+  adminStore: authAdminSlice.reducer,
 });
 export default reducer;
