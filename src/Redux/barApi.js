073025224571019b4053608b4c29c990e3187c1a
farrasmf <farrasmuhammadfurqon@gmail.com>
2024-03-23 15:33:18 +0700

    barchart yah

---
+++ src/Redux/barApi.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = "https://api-car-rental.binaracademy.org/admin";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTcwNTMzMTc5N30.BPM3cOObRc3Kcxs8vGt3OF86J7Ti620px2otHfCCTJo";
+
+const chartOrderReport = ({from, until}) => {
+    const response = axios.get(`${API_URL}/order/reports?from=${from}&until=${until}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            access_token: token
+        }
+        
+    })
+    
+    return response
+}
+
+
+const orderAPI = {
+    
+    chartOrderReport
+};
+
+export default orderAPI;
--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import carSlice from "./carSlice";
 import orderSlice from "./orderSlice";
+import barSlice from "./barSlice";
 
 const reducer = combineReducers({
   car: carSlice,
-  orders:orderSlice
+  orders:orderSlice,
+  orderbarSlice:barSlice
 });
 export default reducer;
